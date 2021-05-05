878ee269f2530a213b81b37c5b5d51a31fe8377f
yoshitaka-tanaka0522 <yositana5227@gmail.com>
2021-05-05 17:51:30 +0900

    vendor追加してみる

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '14cb1404b1bd13f75c41d17e8fa397bf5d52add0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14cb1404b1bd13f75c41d17e8fa397bf5d52add0',
+    ),
+  ),
+);

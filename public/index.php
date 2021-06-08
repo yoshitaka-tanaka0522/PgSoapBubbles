247b1bc331e23c58635f1e0ef99365d5f52455bb
yoshitaka-tanaka0522 <yositana5227@gmail.com>
2021-06-09 01:42:55 +0900

    にしはしのコメントを入れました、修正しました

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
+| にしはし好きだ！！！！！(^^♪(^^♪(^^♪Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
